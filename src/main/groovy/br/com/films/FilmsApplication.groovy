47c38a85c5521429a9ee967fde86b93da87dbe74
paulo@ubots.com.br <f8c45def3e1bf16038cc7be4f068e590ea3fa826>
2020-12-27 09:41:19 -0300

    first commit

---
+++ src/main/groovy/br/com/films/FilmsApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.films
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FilmsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FilmsApplication, args)
+	}
+
+}
